f4b2cd41a259af43379a1aee011e92abd25d1639
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-09-24 17:53:47 +0600

    Connect Database

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 const app = require("./app");
 
 const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DATABASE_URL, () => {
+  console.log("database connect successfully");
+});
 
 app.listen(port, () => {
   console.log(`Tour management server is working...`);
